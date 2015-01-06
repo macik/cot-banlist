0e594f25dce058ead31d3dc61a3618758542c08b
Andrew Matsovkin <macik.spb@gmail.com>
2015-01-06 07:02:41 +0300

    Version bump
    Version number update
    + minor l18n fixes

--- lang/banlist.en.lang.php
+++ lang/banlist.en.lang.php
@@ -10,7 +10,7 @@
 
 defined('COT_CODE') or die('Wrong URL.');
 
-$L['info_desc'] = 'Administration tool to ban users by IP or E-mail';
+$L['info_desc'] = 'Administration tool to ban users by IP/E-mail/login';
 
 /**
  * Plugin Body
@@ -24,7 +24,7 @@ $L['banlist_duration'] = 'Duration';
 $L['banlist_neverexpire'] = 'Never expire';
 
 $L['banlist_help'] = 'Samples for IP masks: 194.31.13.41, 194.31.13.*, 194.31.*.*, 194.*.*.*<br />Samples for email masks: @hotmail.com, @yahoo (Wildcards are not supported)<br />A single entry can contain one IP mask or one email mask or both.<br />IPs are filtered for each and every page displayed, and email masks at user registration only.';
-$L['aut_emailbanned'] = 'This user (or this host) is banned, reason is: ';
+$L['aut_emailbanned'] = 'This login is banned, reason is: ';
 
 $L['banlist_blocked_ip'] = 'Your IP address is banned';
 $L['banlist_blocked_email'] = 'Your e-mail address is banned';
